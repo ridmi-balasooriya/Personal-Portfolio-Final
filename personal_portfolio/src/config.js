df21f3a5bb217a4fcfea0339315927434e0ca548
ridmi-balasooriya <ridmi.bizy@gmail.com>
2024-08-14 18:49:56 +0400

    Update frontend config.js file with new backend API Base URL

--- personal_portfolio/src/config.js
+++ personal_portfolio/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL =    'http://portfolio-env.eba-nwhau5pt.eu-west-1.elasticbeanstalk.com' || 'http://127.0.0.1:8000';
+const API_BASE_URL =    'http://env-portfolio.eba-nwhau5pt.eu-west-1.elasticbeanstalk.com' || 'http://127.0.0.1:8000';
 export default API_BASE_URL;
